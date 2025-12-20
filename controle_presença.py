86cc89f23a048ccb87482639058fe3f38da66924
Hi there! I'm João Paulo <jp-pedrosa@hotmail.com>
2025-12-20 12:37:34 +0000

    Refactor presence tracking and clear screen function

--- controle_presença.py
+++ controle_presença.py
@@ -5,7 +5,11 @@
 # Função para marcar presença.
 
 # Informar total de presentes e ausentes.
+import os
 
+def limpar_tela():
+    os.system('cls' if os.name == 'nt' else 'clear')
+        
 presenca = {
         "Lais": False,
         "Maria": False,
@@ -35,9 +39,8 @@ def marcar_presenca(presenca):
         else:
             presenca[aluno] = False
             ausentes +=1
-        
+     
     
-
     # global presentes, ausentes
     # presentes = 0
     # ausentes = 0
@@ -58,16 +61,7 @@ def marcar_presenca(presenca):
     #             ausentes += 1
     #             break
 
-
-
-import os
-
-def limpar_tela():
-    os.system('cls' if os.name == 'nt' else 'clear')
-
-
 limpar_tela()
-
 
 marcar_presenca(presenca)
 # while True:
@@ -78,7 +72,6 @@ marcar_presenca(presenca)
 #         break
 #     if resp == "N":
 #         break
-
 
 for nome, presente_ou_ausente in presenca.items():
     print('=-' *20)
